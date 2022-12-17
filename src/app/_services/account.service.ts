8bbe61f0dff772c7a6bff05e1077ad981c791102
Karan Chadha <karan.chadha-ext@jci.com>
2022-12-17 22:31:18 +0530

    merge check

--- src/app/_services/account.service.ts
+++ src/app/_services/account.service.ts
@@ -10,7 +10,6 @@ import { User } from '../_models/student.model';
 })
 export class AccountService {
 
-  //github merg check
   private userSubject: BehaviorSubject<User>;
   public user: Observable<User>;
   /**
